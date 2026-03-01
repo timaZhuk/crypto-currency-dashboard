0bfb292e753b77b70dff36b124952f7769e7bf92
unknown <timaZhuk>
2026-03-01 20:59:25 +0500

    Home page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
 import React from "react";
 
 const page = () => {
-  return <div className="text-3xl text-indigo-500">page</div>;
+  return (
+    <main className="main-conatainer">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+              alt="Bitcoin"
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>BitCoin / BTC</p>
+              <h1>$89,113.00</h1>
+            </div>
+          </div>
+        </div>
+
+        <p>Trending Coins</p>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
 };
 
 export default page;
