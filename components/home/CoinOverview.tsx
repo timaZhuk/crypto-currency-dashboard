ef74dda4faa371e28948b0f282ac7bcdd5173f37
unknown <timaZhuk>
2026-03-10 23:03:02 +0500

    implement candlestick and bar chart

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -2,31 +2,51 @@ import { fetcher } from "@/lib/coingecko.actions";
 import { formatCurrency } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import { CoinOverviewFallback } from "./fallbacks";
+import CandlestickChart from "../CandlestickChart";
 
 const CoinOverview = async () => {
   //----symbol of coin
-  const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
-    dex_pair_format: "symbol",
-  });
-  return (
-    <div id="coin-overview">
-      <div className="header pt-2">
-        <Image
-          src={coin.image?.large}
-          alt={coin.name}
-          width={56}
-          height={56}
-          unoptimized
-        />
-        <div className="info">
-          <p>
-            {coin.name} / {coin.symbol.toUpperCase()}
-          </p>
-          <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-        </div>
+
+  try {
+    //run it in parallel with Promises
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: "1", // Можно строкой или числом
+        // precision: 'full' можно оставить, если он вам критичен
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+          <div className="header pt-2">
+            <Image
+              src={coin?.image?.large}
+              alt={coin?.name}
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>
+                {coin.name} / {coin.symbol.toUpperCase()}
+              </p>
+              <h1>
+                {formatCurrency(coin?.market_data?.current_price?.usd || 0)}
+              </h1>
+            </div>
+          </div>
+        </CandlestickChart>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching coin overview: ", error);
+    return <CoinOverviewFallback />;
+  }
 };
 
 export default CoinOverview;
